b9625e029e58699e6c107562e9403d67f31e1227
karaxuna <kaxabernet@gmail.com>
2016-03-31 13:17:37 +0400

    Demo created

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    shell = require('gulp-shell');
 
-gulp.task('build', function() {
+gulp.task('install-bower-packages', function () {
+    return gulp.src('').pipe(shell('"./node_modules/.bin/bower" install --allow-root'));
+});
+
+gulp.task('minify', function() {
     return gulp.src('./fixedTableHeader.js')
         .pipe(uglify())
         .pipe(rename({
             suffix: '.min'
         }))
         .pipe(gulp.dest('./'));
+});
+
+gulp.task('build', ['install-bower-packages', 'minify'], function() {
+    
 });
